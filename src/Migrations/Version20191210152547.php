7a4f5f62463e9c22a9c81c4bfabdf6cc29f17325
shkurik <oleksandr.shkuropat@gmail.com>
2019-12-10 17:31:23 +0200

    php bin/console make:migration

---
+++ src/Migrations/Version20191210152547.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20191210152547 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return 'my first migration in symfony';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE note ADD description VARCHAR(1200) DEFAULT NULL, ADD status SMALLINT DEFAULT NULL, ADD datetime DATETIME DEFAULT NULL, CHANGE name title VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE note DROP description, DROP status, DROP datetime, CHANGE title name VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`');
+    }
+}
